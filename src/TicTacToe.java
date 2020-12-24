efba21f055ce4af6bad5d9fe3b3a29d2fb548c75
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-12-23 19:23:22 -0600

    added sout

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,6 +7,8 @@ public class TicTacToe {
 
 
     public static void main(String[] args) {
+        System.out.println("Let's play TIC TAC TOE");
+
 
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
                             {'-', '+', '-', '+', '-'},
